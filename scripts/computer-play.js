27b285c499573e5ce26e205eaf872230c46e28ea
Matheus Gabriel <71954064+mathzxs@users.noreply.github.com>
2023-02-06 11:30:26 +0000

    Update computer-play.js

--- scripts/computer-play.js
+++ scripts/computer-play.js
@@ -2,5 +2,10 @@ const options = ['rock', 'paper', 'scissors'];
 function getComputerChoice() {
   return options[Math.floor(Math.random() * options.length)];
 }
-  const playerChoice = prompt('Choose rock, paper, or scissors:');
+  const playerChoice = prompt('rock, paper, or scissors:');
 console.log(playRound(playerChoice));
+const x = playerChoice
+  if  (x != options){
+ return false
+ console.log ("Invalid character");
+ }
