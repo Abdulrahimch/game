68591bba942677ffbea8ad54fd36551f9ccdceae
abdulraim <abdulrahimcisco92@gmail.com>
2023-02-10 18:46:02 +0300

    refactoring computer-play

--- scripts/computer-play.js
+++ scripts/computer-play.js
@@ -1,53 +1,32 @@
 console.log ("Enter your choice (0 = Rock // 1 = Paper // 2 = Scissors");
 
-function playJokenpo(playerChoice) {
-  
-  let result;
-  let playerScore = 0;
-  let computerScore = 0;
-  
-  for (let round = 0; round < 5; round++) {
-    const computerChoice = Math.floor(Math.random() * 3);
-    playerChoice = Number(prompt("Enter your choice (0 = Rock // 1 = Paper // 2 = Scissors):"));
-    if (playerChoice < 0 || playerChoice > 2) {
-      console.log("Invalid choice. Please enter a number between 0 and 2.");
-      return;
-    }
-      
-    if (playerChoice === computerChoice) {
-      result = "Tie!";
-    } 
-    else if (playerChoice === 0 && computerChoice === 2) {
-      result = "You win this round! Rock beats Scissors.";
-      playerScore++;
-    } 
-    else if (playerChoice === 1 && computerChoice === 0) {
-      result = "You win this round! Paper beats Rock.";
-      playerScore++;
-    } 
-    else if (playerChoice === 2 && computerChoice === 1) {
-      result = "You win this round! Scissors beats Paper.";
-      playerScore++;
-    } 
-    else {
-      result = "You lose this round. Try again.";
-      computerScore++;
-    }
-    
-    console.log(`Round ${round + 1}:`);
-    console.log(`Your choice: ${playerChoice}`);
-    console.log(`Computer's choice: ${computerChoice}`);
-    console.log(result);
-  
-  }
-  
-  if (playerScore > computerScore) {
-    console.log(`You win the game! Final score: ${playerScore} to ${computerScore}.`);
-  } 
-  else if (playerScore < computerScore) {
-    console.log(`You lose the game. Final score: ${playerScore} to ${computerScore}.`);
-  } 
-  else {
-    console.log(`It's a tie! Final score: ${playerScore} to ${computerScore}.`);
-  }
+const allPlayerChoices = {
+  0: 'rock',
+  1: 'paper',
+  2: 'scissors'
+};
+
+const computerChoice = () => {
+  const choices = ["rock", "paper", "scissors"];
+
+  const random = Math.floor(Math.random() * choices.length);
+  return allPlayerChoices[random];
 }
+
+const playerChoice = () => {
+  const choice = prompt("Enter your choice (0 = Rock // 1 = Paper // 2 = Scissors):");
+
+  if (typeof(choice) !== 'number' ||  (0 <= playerChoice < 2)) {
+    console.log("Invalid choice. Please enter a number between 0 and 2.");
+    return;
+  }
+
+  return allPlayerChoices[choice];
+};
+
+
+module.exports = { playerChoice, computerChoice } ;
+
+
+
+
--- scripts/game.js
+++ scripts/game.js
@@ -1,12 +1,13 @@
 const playRound = require('./play-round');
+const { playerChoice, computerChoice } = require('./computer-play');
 
 const game = () => {
     let playerScore = 0,
-    computerScore = 0;
+        computerScore = 0;
 
     for (let round = 0; round < 5; round++) {
-        const playerSelection = 'scissors',
-            computerSelection = 'rock';
+        const playerSelection = playerChoice(),
+              computerSelection = computerChoice();
         playerScore += playRound(playerSelection, computerSelection).playerCounter;
         computerScore += playRound(playerSelection, computerSelection).computerCounter;
         console.log(playRound(playerSelection, computerSelection).response);
@@ -16,7 +17,7 @@ const game = () => {
         console.log(`Congrates  you Wone!!!! your Score: ${playerScore} Computer Score: ${computerScore}`);
     else if (playerScore < computerScore)
         console.log(`Game over  Try again later, your Score: ${playerScore} Computer Score: ${computerScore}`);
-    else console.log(`None won, Try one more tome , your Score: ${playerScore} Computer Score: ${computerScore}`);
+    else console.log(`None won, Try one more time , your Score: ${playerScore} Computer Score: ${computerScore}`);
 }
 
 game();
